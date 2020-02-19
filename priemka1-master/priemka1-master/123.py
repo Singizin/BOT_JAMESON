edf4ed62b81de93227e836d521648fc2ba0e2c6a
igsin <igsing96@gmail.com>
2020-02-19 23:32:15 +0700

    minimal work

--- priemka1-master/priemka1-master/123.py
+++ priemka1-master/priemka1-master/123.py
@@ -1,30 +1,53 @@
 import telebot
 
-telebot.apihelper.proxy = {'https':'https://176.241.138.70:50003'}
+telebot.apihelper.proxy = {'https': 'https://51.158.111.229:8811'}
 
 bot = telebot.TeleBot('854025714:AAH9Wi3_rWfVJvjnbDgNWkL8hYCbH2Fr-wY')
+
+place = 'Нур-Султан, ул. Акмешит, д.14\n' \
+        'Склад\n'
+
+cities = ['Нур-Султан', 'Павлодар']
+cities_low = []
+
+for i in cities:
+    cities_low.append(i.lower())
+
+alco = ['Jameson', 'Сhivas']
+alco_low = []
+
+for i in alco:
+    alco_low.append(i.lower())
+
 keyboard1 = telebot.types.ReplyKeyboardMarkup()
 
-alco = ['Виски "Jameson"', 'Зеленая марка','777','blazer']
-for i in alco:
-    keyboard1.row(i)
-bot.send_message(chat_id = 215482021, text = 'ты пидор')
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Давай найдем алкоголь! Что тебе нужно?', reply_markup=keyboard1)
+    bot.send_message(message.chat.id, 'Где вы находитесь?', reply_markup=keyboard1)
+    keyboard1.keyboard.clear()
+    for i in cities:
+        keyboard1.row(i)
+
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    print(message)
-    if message.text.lower() == 'виски \"jameson\"':
-        bot.send_message(message.chat.id, 'в Ашане')
-    elif message.text.lower() == 'зеленая марка':
-        bot.send_message(message.chat.id, 'У казахов на втором этаже')
-    elif message.text.lower() == 'я тебя люблю':
-        bot.send_sticker(message.chat.id, 'CAADAgADZgkAAnlc4gmfCor5YbYYRAI')
+    if message.text.lower() in cities_low:
+        bot.send_message(message.chat.id, 'Что ищете?', reply_markup=keyboard1)
+        keyboard1.keyboard.clear()
+        for i in alco:
+            keyboard1.row(i)
+    elif message.text.lower() in alco_low:
+        bot.send_message(message.chat.id, place, reply_markup=keyboard1)
+        bot.send_contact(message.chat.id, phone_number='+79996664444',
+                         first_name='Нурсултан', last_name='Назарбаев')
+        keyboard1.keyboard.clear()
+        keyboard1.row('/start')
+
 
 @bot.message_handler(content_types=['sticker'])
 def sticker_id(message):
     print(message)
+
 
 bot.polling()
