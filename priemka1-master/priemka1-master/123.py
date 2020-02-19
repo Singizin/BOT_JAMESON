4c2e6348bf4601e68b4677cb5651487e80202513
igsin <igsing96@gmail.com>
2020-02-19 23:49:57 +0700

    minimal work

--- priemka1-master/priemka1-master/123.py
+++ priemka1-master/priemka1-master/123.py
@@ -2,10 +2,10 @@ import telebot
 
 telebot.apihelper.proxy = {'https': 'https://51.158.111.229:8811'}
 
-bot = telebot.TeleBot('854025714:AAH9Wi3_rWfVJvjnbDgNWkL8hYCbH2Fr-wY')
+bot = telebot.TeleBot('807872124:AAE584RSfGzekXBOF84j1309iNUXE74L7Yg')
 
 place = 'Нур-Султан, ул. Акмешит, д.14\n' \
-        'Склад\n'
+        'Склад'\
 
 cities = ['Нур-Султан', 'Павлодар']
 cities_low = []
@@ -24,25 +24,25 @@ keyboard1 = telebot.types.ReplyKeyboardMarkup()
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Где вы находитесь?', reply_markup=keyboard1)
     keyboard1.keyboard.clear()
     for i in cities:
         keyboard1.row(i)
+    bot.send_message(message.chat.id, 'Где вы находитесь?', reply_markup=keyboard1)
 
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     if message.text.lower() in cities_low:
-        bot.send_message(message.chat.id, 'Что ищете?', reply_markup=keyboard1)
         keyboard1.keyboard.clear()
         for i in alco:
             keyboard1.row(i)
+        bot.send_message(message.chat.id, 'Что ищете?', reply_markup=keyboard1)
     elif message.text.lower() in alco_low:
-        bot.send_message(message.chat.id, place, reply_markup=keyboard1)
-        bot.send_contact(message.chat.id, phone_number='+79996664444',
-                         first_name='Нурсултан', last_name='Назарбаев')
         keyboard1.keyboard.clear()
         keyboard1.row('/start')
+        bot.send_message(message.chat.id, place, reply_markup=keyboard1)
+        bot.send_contact(message.chat.id, phone_number='+79996664444', first_name='Нурсултан', last_name='Назарбаев')
+
 
 
 @bot.message_handler(content_types=['sticker'])
