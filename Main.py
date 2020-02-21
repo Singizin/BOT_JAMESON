6a734404c84ae237766e86c1082b32bc26bb3507
Singizin <48468145+Singizin@users.noreply.github.com>
2020-02-21 22:00:15 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import telebot
 
-telebot.apihelper.proxy = {'https': 'https://51.158.111.229:8811'}
+#telebot.apihelper.proxy = {'https': 'https://51.158.111.229:8811'}
 
 bot = telebot.TeleBot('807872124:AAE584RSfGzekXBOF84j1309iNUXE74L7Yg')
 
@@ -46,9 +46,14 @@ def send_text(message):
         bot.send_message(message.chat.id, 'Что ищете?', reply_markup=keyboard1)
     elif message.text.lower() in alco_low:
         keyboard1.keyboard.clear()
-        keyboard1.row('/start')
+        keyboard1.row('найти новую позицию')
         bot.send_message(message.chat.id, a, reply_markup=keyboard1)
-        #bot.send_contact(message.chat.id, phone_number='+79996664444', first_name='Нурсултан', last_name='Назарбаев')
+    elif message.text.lower() == 'найти новую позицию':
+        keyboard1.keyboard.clear()
+        for i in cities:
+            keyboard1.row(i)
+        bot.send_message(message.chat.id, 'Где вы находитесь?', reply_markup=keyboard1)
+                #bot.send_contact(message.chat.id, phone_number='+79996664444', first_name='Нурсултан', last_name='Назарбаев')
 
 
 
